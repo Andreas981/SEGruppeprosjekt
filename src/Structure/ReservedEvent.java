d522588328dee9bf16210a195b62f6f15ba73375
Andreas Mikalsen <amikal@hotmail.no>
2019-02-14 13:19:55 +0100

    Added abstract ReservedEvent

---
+++ src/Structure/ReservedEvent.java
@@ -0,0 +1,7 @@
+package Structure;
+
+public abstract class ReservedEvent extends Event{
+    public ReservedEvent(String name, String organizer, String date, String time, String location) {
+        super(name, organizer, date, time, location);
+    }
+}
