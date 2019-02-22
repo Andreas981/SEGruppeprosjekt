138d2a3f4a0e5e31da44fc6f46c8dcb3f1284b13
Andreas Mikalsen <amikal@hotmail.no>
2019-02-22 16:54:46 +0100

    Variables in Room

--- src/main/java/Model/Room.java
+++ src/main/java/Model/Room.java
@@ -1,4 +1,39 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Room {
+    String name;
+    int maxParticipents;
+    Boolean eventInProgress;
+    ArrayList<SeatedEvent> events;
+
+    public Room(String name, int maxParticipents, Boolean eventInProgress) {
+        this.name = name;
+        this.maxParticipents = maxParticipents;
+        this.eventInProgress = eventInProgress;
+        this.events = new ArrayList<SeatedEvent>();
+    }
+
+    
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getMaxParticipents() {
+        return maxParticipents;
+    }
+    public void setMaxParticipents(int maxParticipents) {
+        this.maxParticipents = maxParticipents;
+    }
+    public Boolean getEventInProgress() {
+        return eventInProgress;
+    }
+    public void setEventInProgress(Boolean eventInProgress) {
+        this.eventInProgress = eventInProgress;
+    }
 }
