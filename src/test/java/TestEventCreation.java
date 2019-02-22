264dad3b8fdf76e7f0ad8813bba6f7e75d843aef
Andreas Nielsen <andreni@hiof.no>
2019-02-22 15:56:32 +0100

    Auto stash before merge of "Creating_Model" and "origin/Creating_Model"

---
+++ src/test/java/TestEventCreation.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.Time;
+import java.util.Date;
+
+
+
+public class TestEventCreation {
+
+    @Test
+    public void objectInstanceCreation(){
+        PlannedEvent plannedEvent = new PlannedEvent("TestEvent",new Date(2019,4,4),new Time(22,2,22),90,18 );
+        Assert.assertEquals(plannedEvent,plannedEvent);
+    }
+}
