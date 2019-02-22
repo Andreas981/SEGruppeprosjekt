a5ba4bb51d47e89050f51dfe918c75d404a26767
Andreas Mikalsen <amikal@hotmail.no>
2019-02-22 16:10:17 +0100

    Added Organizer only fields

--- src/main/java/Model/Organizer.java
+++ src/main/java/Model/Organizer.java
@@ -7,7 +7,24 @@ public class Organizer extends User{
     String organization;
     int accessLevel;
 
-    public Organizer(String firstName, String lastName, String mail, String telephone, String username, String password, Date birthday) {
+    public Organizer(String firstName, String lastName, String mail, String telephone, String username, String password, Date birthday, String organization, int  accessLevel) {
         super(firstName, lastName, mail, telephone, username, password, birthday);
+        this.organization = organization;
+        this.accessLevel = accessLevel;
+    }
+
+
+
+    public String getOrganization() {
+        return organization;
+    }
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+    public int getAccessLevel() {
+        return accessLevel;
+    }
+    public void setAccessLevel(int accessLevel) {
+        this.accessLevel = accessLevel;
     }
 }
