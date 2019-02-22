1175ebedf3c57736d52664c18a10323aceb0df9d
Andreas Mikalsen <amikal@hotmail.no>
2019-02-22 16:13:37 +0100

    Added tests for organizer

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -39,4 +39,28 @@ public abstract class User {
     public void setBirthday(Date birthday) {
         this.birthday = birthday;
     }
+    public String getMail() {
+        return mail;
+    }
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    public String getTelephone() {
+        return telephone;
+    }
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/test/java/User.java
+++ src/test/java/User.java
@@ -12,7 +12,7 @@ public class User {
 
     @Before
     public void settingUpOrganizer(){
-        organizer = new Organizer("Andreas", "Mikalsen", "amilal@hotmail.no", "97781816", "andremi","abc123", new Date(1998-1900, 6-1, 12));
+        organizer = new Organizer("Andreas", "Mikalsen", "amilal@hotmail.no", "97781816", "andremi","abc123", new Date(1998-1900, 6-1, 12), "HiØ", 1);
     }
     @Test
     public void gettingFullNameOfOrganizer(){
@@ -25,6 +25,14 @@ public class User {
     @Test
     public void gettingTheBirthdayOfOrganizer(){
         Assert.assertEquals("1998-06-12", organizer.getBirthday().toString());
+    }
+    @Test
+    public void gettingAccessLevelFromOrganizer(){
+        Assert.assertEquals(1, organizer.getAccessLevel());
+    }
+    @Test
+    public void gettingUserNameFromOrganizer(){
+        Assert.assertEquals("andremi", organizer.getUsername());
     }
 
 }
