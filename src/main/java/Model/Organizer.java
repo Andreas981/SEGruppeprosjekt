54511a4377eb51dd9f2eecfeff3831995c9a40c2
Andreas Mikalsen <amikal@hotmail.no>
2019-02-22 16:04:24 +0100

    Added fields for organizer

--- src/main/java/Model/Organizer.java
+++ src/main/java/Model/Organizer.java
@@ -4,6 +4,9 @@ import java.sql.Date;
 
 public class Organizer extends User{
 
+    String organization;
+    int accessLevel;
+
     public Organizer(String firstName, String lastName, String mail, String telephone, String username, String password, Date birthday) {
         super(firstName, lastName, mail, telephone, username, password, birthday);
     }
--- src/test/java/User.java
+++ src/test/java/User.java
@@ -14,18 +14,14 @@ public class User {
     public void settingUpOrganizer(){
         organizer = new Organizer("Andreas", "Mikalsen", "amilal@hotmail.no", "97781816", "andremi","abc123", new Date(1998-1900, 6-1, 12));
     }
-
-
     @Test
     public void gettingFullNameOfOrganizer(){
         Assert.assertEquals("Andreas Mikalsen", organizer.getFullName());
     }
-
     @Test
     public void gettingWringNameFromOrganizer(){
         Assert.assertNotEquals("HAHAHA", organizer.getFullName());
     }
-
     @Test
     public void gettingTheBirthdayOfOrganizer(){
         Assert.assertEquals("1998-06-12", organizer.getBirthday().toString());
