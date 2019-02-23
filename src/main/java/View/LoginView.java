ba77223833a8498bb104f58fa148bb583c5390f0
Andreas Mikalsen <amikal@hotmail.no>
2019-02-23 13:01:39 +0100

    Pseudo code for login

--- src/main/java/View/LoginView.java
+++ src/main/java/View/LoginView.java
@@ -1,4 +1,13 @@
 package View;
 
 public class LoginView {
+    /*
+
+    - Ask for which role the user is
+    - Prompt for username
+    - Prompt for password
+    - Check if it's a match
+
+
+     */
 }
