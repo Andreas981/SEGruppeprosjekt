6a4f073fc4e082bf39d63b24dd34dde96377d5d0
Andreas Nielsen <andreni@hiof.no>
2019-03-01 16:49:10 +0100

    Added ordercontroller

--- src/main/java/Controller/OrderController.java
+++ src/main/java/Controller/OrderController.java
@@ -1,4 +1,22 @@
 package Controller;
 
+import Model.SeatedPlannedEvent;
+
+import java.util.Scanner;
+
 public class OrderController {
+    private Scanner scanner;
+    /*
+        Assuming a customer is logged in.
+        Have entered a location(SeatedEvent) or a NonSeatedEvent
+     */
+
+    public void enterPurchaseMode(){
+        // D
+
+        // Get the selected event the user wants to get tickets too.
+        int selectedEvent = Integer.parseInt(scanner.next());
+
+
+    }
 }
