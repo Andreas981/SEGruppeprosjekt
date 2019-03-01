6d55a5b2241cb355e3baaa9af3fd0ca24d740453
Andreas Nielsen <andreni@hiof.no>
2019-03-01 12:35:26 +0100

    Refactored Register View and controller

--- src/main/java/Controller/RegisterUserController.java
+++ src/main/java/Controller/RegisterUserController.java
@@ -2,14 +2,123 @@ package Controller;
 
 import Dummy.Database;
 import Model.Customer;
+import View.RegisterUserView;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 public class RegisterUserController {
+    // Fields collected
+    private String userName;
+    private String userFirstName;
+    private String userLastName;
+    private String userEmail;
+    private String userPhoneNumber;
+    private String userPassword;
+    private Date userBirthDay;
+    private RegisterUserView registerUserView;
+    private Scanner scanner;
 
-    public static Boolean userNameIsValid(String userName){
+    public RegisterUserController(){
+        registerUserView = new RegisterUserView();
+        scanner = new Scanner(System.in);
+    }
+
+    public void startRegistration(){
+        registerUserView.displayPromptForUserName();
+        userName = scanner.next();
+        if(userName.length()>5){
+            if(isUserNameIsValid(userName)){
+                askForFirstname();
+            }else{
+                registerUserView.displayErrorToUser("You have selected a invalid username");
+                startRegistration();
+            }
+        }else{
+            registerUserView.displayErrorToUser("Username must at least contain 5 characters");
+            startRegistration();
+        }
+    }
+
+    private void askForFirstname() {
+        registerUserView.displayPromptForUserFirstname();
+        userFirstName = scanner.next();
+        if(userFirstName.length()>1){
+            askForLastname();
+        }else{
+            registerUserView.displayErrorToUser("Please input a first name");
+            askForFirstname();
+        }
+    }
+
+    private void askForLastname() {
+        registerUserView.displayPromptForUserLastname();
+        userLastName = scanner.next();
+        if(userLastName.length()>1){
+            askForUserEmail();
+        }else{
+            registerUserView.displayErrorToUser("Please input a last name");
+            askForLastname();
+        }
+    }
+
+    private void askForUserEmail() {
+        registerUserView.displayPromptForUserEmail();
+        userEmail = scanner.next();
+        if(userEmail.length()>1){
+            if(mailIsValid(userEmail)){
+                askForUserPhoneNumber();
+            }else{
+                registerUserView.displayErrorToUser("You already have a account registered with this email");
+            }
+
+        }else{
+            registerUserView.displayErrorToUser("Please input a valid email");
+        }
+    }
+
+    private void askForUserPhoneNumber() {
+        registerUserView.displayPromptForUserPhoneNumber();
+        userPhoneNumber = scanner.next();
+        if(userEmail.length()>=8){
+            askForUserBirthDate();
+        }else{
+            registerUserView.displayErrorToUser("Please input a valid phone number");
+            askForUserPhoneNumber();
+        }
+    }
+
+    private void askForUserBirthDate() {
+        registerUserView.displayPromptForBirthdate();
+        String birthDayEntered = scanner.next();
+        if(tryParseInputDate(birthDayEntered)){
+            userBirthDay = parseInputDate(birthDayEntered);
+            askForUserPassword();
+        }else{
+            registerUserView.displayErrorToUser("Invalid date entered! \n" +
+                    "Enter it like 1980-02-01");
+            askForUserBirthDate();
+        }
+    }
+
+    // TODO Password validation method
+    private void askForUserPassword() {
+        registerUserView.displayPromptForUserPassword();
+        userPassword = scanner.next();
+        if(userPassword.length()>=5){
+            registerUserIntoDatabase(userFirstName,userLastName,userEmail,userPhoneNumber,
+                    userName,userPassword,userBirthDay);
+        }else{
+            registerUserView.displayErrorToUser("Please input a stronger password");
+            askForUserPassword();
+        }
+    }
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+    public Boolean isUserNameIsValid(String userName){
         // Search through database to see if userName exist
         for (int i = 0; i < Database.customers.size(); i++){
             if (Database.customers.get(i).getUsername().equals(userName)){
@@ -31,7 +140,21 @@ public class RegisterUserController {
         return true;
     }
 
-    public static Date parseInputDate(String dateEntered){
+    public static boolean tryParseInputDate(String dateEntered){
+        SimpleDateFormat birthDay = new SimpleDateFormat("dd-MM-yyyy");
+        Date dateParsed = null;
+        try {
+            //Parsing the String
+            dateParsed = birthDay.parse(dateEntered);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            System.out.println("Invalid date input! Should be Day-Month-Year");
+            return false;
+        }
+        return true;
+    }
+
+    private Date parseInputDate(String dateEntered){
         SimpleDateFormat birthDay = new SimpleDateFormat("dd-MM-yyyy");
         Date dateParsed = null;
         try {
@@ -47,7 +170,7 @@ public class RegisterUserController {
 
 
 
-    public static boolean registerUserIntoDatabase(String firstName, String lastName, String mail
+    private boolean registerUserIntoDatabase(String firstName, String lastName, String mail
             , String telephone, String username, String password, Date birthday){
         try {
             Database.customers.add(new Customer(firstName,lastName,mail
--- src/main/java/View/RegisterUserView.java
+++ src/main/java/View/RegisterUserView.java
@@ -9,6 +9,46 @@ import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class RegisterUserView {
+
+    public RegisterUserView(){
+
+    }
+
+    public void displayPromptForUserName() {
+        System.out.println("Please enter a desired username:");
+    }
+
+    public void displayErrorToUser(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+
+    public void displayPromptForUserFirstname() {
+        System.out.println("Please enter your firstname:");
+    }
+
+    public void displayPromptForUserLastname() {
+        System.out.println("Please enter your lastname:");
+    }
+
+    public void displayPromptForUserEmail() {
+        System.out.println("Please enter your email:");
+    }
+
+    public void displayPromptForUserPhoneNumber() {
+        System.out.println("Please enter a valid phone number:");
+    }
+
+    public void displayPromptForUserPassword() {
+        System.out.println("Please type in a valid password:");
+    }
+
+    public void displayPromptForBirthdate(){
+        System.out.println("Please enter a valid birth date. \n" +
+                "In format: DD-MM-YYYY");
+    }
+
+}
+    /*
     public String createNewUser() {
         // Anyone can create a customer account
         // For creating a organizer, an administrator account must be used.
