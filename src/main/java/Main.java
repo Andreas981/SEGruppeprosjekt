0899ec82e108b3bf618157db64f1969c9f6811f5
Andreas Mikalsen <amikal@hotmail.no>
2019-03-03 11:59:51 +0100

    Input validation in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import Controller.LogInUserController;
 import Controller.RegisterUserController;
 import Dummy.*;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -20,30 +21,35 @@ public class Main {
         Boolean exit = false;
         Scanner scanner = new Scanner(System.in);
         printMenu();
-        // TODO input validation
 
-        int userSelection = scanner.nextInt();
-        switch (userSelection) {
-            case 1:
-                new LogInUserController().initLogin();
-                break;
-            case 2:
-                RegisterUserController registerAnewUser = new RegisterUserController();
-                registerAnewUser.startRegistrationForUser();
-                displayMenu();
-                break;
-            case 3:
-                CustomerMenuController.displayOnGoingEvents();
-                displayMenu();
-                break;
-            case 4:
-                System.out.println("Goodbye");
-                exit = true;
-                break;
-            default:
-                System.out.println("Something went wrong");
-                printMenu();
+        int userSelection = -1;
+        try{
+            userSelection = scanner.nextInt();
+            switch (userSelection) {
+                case 1:
+                    new LogInUserController().initLogin();
+                    break;
+                case 2:
+                    RegisterUserController registerAnewUser = new RegisterUserController();
+                    registerAnewUser.startRegistrationForUser();
+                    displayMenu();
+                    break;
+                case 3:
+                    CustomerMenuController.displayOnGoingEvents();
+                    displayMenu();
+                    break;
+                case 4:
+                    System.out.println("Goodbye");
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Something went wrong");
+                    printMenu();
+            }
+        }catch (InputMismatchException e){
+            System.out.println("Sorry, that is not an option");
         }
+
         if(!exit){
             displayMenu();
         }
