6d52a30153298ecae3a168d611a7e3d59005d3a2
Andreas Mikalsen <amikal@hotmail.no>
2019-03-03 12:07:39 +0100

    Did TODOs in NonSeatedPlannedEvent

--- src/main/java/Model/NonSeatedPlannedEvent.java
+++ src/main/java/Model/NonSeatedPlannedEvent.java
@@ -2,24 +2,65 @@ package Model;
 
 import org.joda.time.LocalDateTime;
 
+import java.util.ArrayList;
+
 
 public class NonSeatedPlannedEvent extends PlannedEvent {
     private int freeSpace;
     private String meetUp;
+    private Boolean freeEvent;
+    private ArrayList<Customer> participents;
+    private ArrayList<String> specialNotices;
 
     // TODO Modified constructor for different events:
 
-    public NonSeatedPlannedEvent(String nameOfEvent, LocalDateTime dateOfEvent, int lengthOfEvent, int ageLimit, int freeSpace, String meetUp) {
+    public NonSeatedPlannedEvent(String nameOfEvent, LocalDateTime dateOfEvent, int lengthOfEvent, int ageLimit, int freeSpace, String meetUp, Boolean freeEvent) {
         super(nameOfEvent, dateOfEvent, lengthOfEvent, ageLimit);
         this.freeSpace = freeSpace;
         this.meetUp = meetUp;
+        this.freeEvent = freeEvent;
+        participents = new ArrayList<Customer>();
+        specialNotices = new ArrayList<String>();
     }
 
-    // TODO Field for list of specialNotice
-    // TODO field for boolean that can be checked if the event is free
-    // TODO Allow users with an account to be added to participants ArrayList<customer>
+    public void addParticipent(Customer customer){
+        participents.add(customer);
+    }
+    public void removeParticipents(int index){
+        participents.remove(index);
+    }
+    public ArrayList<Customer> getParticipents(){
+        return participents;
+    }
+
+    public void addNotice(String notice){
+        specialNotices.add(notice);
+    }
+    public void removeNotice(int index){
+        specialNotices.remove(index);
+    }
+    public ArrayList<String> getNotices(){
+        return specialNotices;
+    }
+
+    public int getFreeSpace() {
+        return freeSpace;
+    }
+    public void setFreeSpace(int freeSpace) {
+        this.freeSpace = freeSpace;
+    }
+    public String getMeetUp() {
+        return meetUp;
+    }
+    public void setMeetUp(String meetUp) {
+        this.meetUp = meetUp;
+    }
+    public Boolean getFreeEvent() {
+        return freeEvent;
+    }
+    public void setFreeEvent(Boolean freeEvent) {
+        this.freeEvent = freeEvent;
+    }
 
 
-    // TODO Method for displaying all notice's regarding the event
-    // TODO Method for adding a notice to the event
 }
