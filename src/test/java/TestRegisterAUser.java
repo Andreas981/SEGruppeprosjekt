c8572c7b76fc00a4c4edd9532431685249925a9c
Andreas Nielsen <andreni@hiof.no>
2019-03-12 17:51:01 +0100

    Check for username testcase

--- src/test/java/TestRegisterAUser.java
+++ src/test/java/TestRegisterAUser.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import Controller.RegisterUserController;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class TestRegisterAUser {
+    @Before
+    public void init(){
+        RegisterUserController registerUserController = new RegisterUserController();
+    }
+
+    @Test
+    public void userNameIsValid(){
+        Assert.assertEquals(true,"persen");
+    }
+
+
 }
+
