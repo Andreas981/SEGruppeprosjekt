f9e6ceff6572f2f2278379435cda49266c09ce8c
Andreas Mikalsen <amikal@hotmail.no>
2019-03-12 17:39:10 +0100

    Refactored location view

--- src/main/java/View/LevelTwoOrganizerView.java
+++ src/main/java/View/LevelTwoOrganizerView.java
@@ -2,6 +2,7 @@ package View;
 
 import Controller.LevelTwoOrganizerController;
 import Controller.LocationController;
+import Controller.RoomController;
 import Dummy.Database;
 
 import java.util.InputMismatchException;
@@ -10,17 +11,22 @@ import java.util.Scanner;
 public class LevelTwoOrganizerView {
 
     private Scanner scanner = new Scanner(System.in);
-    private LevelTwoOrganizerController organizerController = new LevelTwoOrganizerController();
+    private LocationController locationController = new LocationController();
+    private RoomController roomController = new RoomController();
+    private int currentLocation = -1;
+    private int currentRoom = -1;
 
     public void startView() {
         System.out.println("This is your registred locations: ");
-        organizerController.printLocationForCurrenOrganizer();
+        for(int i = 0; i < Database.currentLoggedInOrganizer.getLocations().size(); i++)
+            System.out.println("\t(" + i + ") " + Database.currentLoggedInOrganizer.getLocations().get(i).getName());
 
         System.out.println("\nWhat would you like to do?");
         System.out.println("\t(1) See a location");
         System.out.println("\t(2) Add location");
         System.out.println("\t(3) Remove a location");
-        System.out.println("\t(4) Sign out");
+        System.out.println("\t(4) Edit room");
+        System.out.println("\t(5) Sign out");
 
         int choice = 0;
         try {
@@ -36,6 +42,9 @@ public class LevelTwoOrganizerView {
                     removeLocation();
                     break;
                 case 4:
+                    editLocation();
+                    break;
+                case 5:
                     Database.currentLoggedInOrganizer = null;
                     return;
                 default:
@@ -48,7 +57,6 @@ public class LevelTwoOrganizerView {
         startView();
     }
 
-
     private void seeLocation(){
         System.out.println("\nWhich location do you want to see?");
 
@@ -60,11 +68,36 @@ public class LevelTwoOrganizerView {
             startView();
         }
 
-        if(choice > -1 && LocationController.checkExistance(choice)){
-            seeLocationView(choice);
+        if(choice > -1 && locationController.checkExistance(choice)){
+            currentLocation = choice;
+            seeLocationView();
         }else {
             displaySomethingWentWrong();
             startView();
+        }
+
+    }
+    private void addLocation() {
+        System.out.println("\nName of location?");
+        String name = new Scanner(System.in).next();
+        System.out.println("Location address?");
+        String address = new Scanner(System.in).next();
+        System.out.println("Is it a public location? (1)yes (2)no");
+        int publicLocaiton = -1;
+        try{
+            publicLocaiton = new Scanner(System.in).nextInt();
+        }catch (InputMismatchException e){
+            displayNotAnOption();
+        }
+
+        if(publicLocaiton == 1 || publicLocaiton == 2){
+            if(locationController.addLocation(name, address, publicLocaiton)){
+                System.out.println("Location added!");
+                startView();
+            }else{
+                displaySomethingWentWrong();
+                startView();
+            }
         }
 
     }
@@ -82,7 +115,7 @@ public class LevelTwoOrganizerView {
             if (confirm.toLowerCase().startsWith("n")){
                 startView();
             }else if(confirm.toLowerCase().startsWith("y")){
-                if(LocationController.removeLocation(choice)){
+                if(locationController.removeLocation(choice)){
                     System.out.println("Location removed!");
                 }else{
                     displaySomethingWentWrong();
@@ -94,53 +127,254 @@ public class LevelTwoOrganizerView {
             startView();
         }
     }
-    private void addLocation() {
-        System.out.println("\nName of location?");
-        String name = new Scanner(System.in).next();
-        System.out.println("Location address?");
-        String address = new Scanner(System.in).next();
-        System.out.println("Is it a public location? (1)yes (2)no");
-        int publicLocaiton = -1;
+    private void editLocation(){
+        System.out.println("\nWhich location do you want to remove?");
+
+        int location = -1;
         try{
-            publicLocaiton = new Scanner(System.in).nextInt();
+            location = scanner.nextInt();
         }catch (InputMismatchException e){
             displayNotAnOption();
+            startView();
         }
 
-        if(publicLocaiton == 1 || publicLocaiton == 2){
-            if(LocationController.addLocation(name, address, publicLocaiton)){
-                System.out.println("Location added!");
-                startView();
-            }else{
-                displaySomethingWentWrong();
-                startView();
-            }
+        if (location >  -1 && location < Database.currentLoggedInOrganizer.getLocations().size()){
+            System.out.println("Type \".\" if you don't want to change a field");
+
+            System.out.println("Name: ");
+            String name = new Scanner(System.in).nextLine();
+
+            System.out.println("Address: ");
+            String address = new Scanner(System.in).nextLine();
+
+            System.out.println("Public location (yes/no): ");
+            String publicLocation = new Scanner(System.in).next();
+
+            locationController.editLocaiton(location, name, address, publicLocation);
+
+            System.out.println("Location edited!");
+        }else{
+            displaySomethingWentWrong();
         }
+        startView();
 
     }
 
-    private void seeLocationView(int location) {
+    private void seeLocationView() {
         System.out.println("\n****************************");
-        System.out.println("Name: " + Database.currentLoggedInOrganizer.getLocations().get(location).getName() +
-                "\nAddress: " + Database.currentLoggedInOrganizer.getLocations().get(location).getAddress() +
-                "\nIs public: " + Database.currentLoggedInOrganizer.getLocations().get(location).getPublicLocation()
+        System.out.println("Name: " + Database.currentLoggedInOrganizer.getLocations().get(currentLocation).getName() +
+                "\nAddress: " + Database.currentLoggedInOrganizer.getLocations().get(currentLocation).getAddress() +
+                "\nIs public: " + Database.currentLoggedInOrganizer.getLocations().get(currentLocation).getPublicLocation()
         );
-        if (Database.currentLoggedInOrganizer.getLocations().get(location).getRooms().size() == 0){
+        if (Database.currentLoggedInOrganizer.getLocations().get(currentLocation).getRooms().size() == 0){
             System.out.println("Thre is no registered rooms for this locaton");
         }else{
             System.out.println("Rooms for this location: ");
-            for(int i = 0; i < Database.currentLoggedInOrganizer.getLocations().get(location).getRooms().size(); i++){
-                System.out.println("\t(" + i + ")" + Database.currentLoggedInOrganizer.getLocations().get(location).getRooms().get(i).getName());
+            for(int i = 0; i < Database.currentLoggedInOrganizer.getLocations().get(currentLocation).getRooms().size(); i++){
+                System.out.println("\t(" + i + ")" + Database.currentLoggedInOrganizer.getLocations().get(currentLocation).getRooms().get(i).getName());
             }
         }
         System.out.println("****************************");
 
-        
 
+        System.out.println("\nWhat would you like to do?");
+        System.out.println("\t(1) See a room");
+        System.out.println("\t(2) Add a room");
+        System.out.println("\t(3) Edit a room");
+        System.out.println("\t(4) Remove a room");
+        System.out.println("\t(5) Go back");
 
+        int choice = 0;
+        try{
+            choice = scanner.nextInt();
+        }catch (InputMismatchException e){
+            displayNotAnOption();
+            seeLocationView();
+        }
+        switch (choice){
+            case 1:
+                seeRoom();
+                break;
+            case 2:
+                addRoom();
+                break;
+            case 3:
+                removeRoom();
+                break;
+            case 4:
+                editRoom();
+                break;
+            case 5:
+                break;
+            default:
+                displayNotAnOption();
+                seeLocationView();
+                break;
+        }
+        seeLocationView();
+
+    }
+
+    private void seeRoom(){
+        System.out.println("\nWhich room do you want to see?");
+
+        int choice = -1;
+        try{
+            choice = scanner.nextInt();
+        }catch (InputMismatchException e){
+            displayNotAnOption();
+            return;
+        }
+
+        if(choice > -1 && choice < Database.currentLoggedInOrganizer.getLocations().get(currentLocation).getRooms().size()){
+            currentRoom = choice;
+            seeRoomView();
+        }else {
+            displayNotAnOption();
+            currentRoom = -1;
+            seeLocationView();
+        }
+
+    }
+    private void addRoom() {
+        System.out.println("\nRoom name: ");
+        String name = new Scanner(System.in).nextLine();
+
+        System.out.println("Max participents: ");
+        int max = -1;
+        try{
+            max = scanner.nextInt();
+        }catch (InputMismatchException e){
+            displayNotAnOption();
+            seeLocationView();
+        }
+        roomController.addRoom(currentLocation, name, max);
+        System.out.println("Room added!");
+
+        seeLocationView();
+    }
+    private void removeRoom() {
+        System.out.println("\nWhich room do you want to remove?");
+
+        int choice = -1;
+        try{
+            choice = scanner.nextInt();
+        }catch (InputMismatchException e){
+            displayNotAnOption();
+            seeLocationView();
+        }
+        System.out.println("Are you sure?");
+        System.out.println("yes(y)/no(n)");
+
+        String confirm = scanner.next();
+        if(confirm.toLowerCase().startsWith("y")){
+            if (choice > -1 && choice < Database.currentLoggedInOrganizer.getLocations().get(currentLocation).getRooms().size()) {
+                roomController.removeRoom(currentLocation, choice);
+                System.out.println("Room removed");
+            }else{
+                displaySomethingWentWrong();
+            }
+        }
+
+        seeLocationView();
 
 
     }
+    private void editRoom(){
+        System.out.println("Which room do you want to edit?");
+        int room;
+        try{
+            room = scanner.nextInt();
+        }catch (InputMismatchException e){
+            displayNotAnOption();
+            return;
+        }
+        if(room >= 0 && room < Database.currentLoggedInOrganizer.getLocations().get(currentLocation).getRooms().size()){
+
+            System.out.println("Write \".\" if you dont want to change a field");
+            System.out.println("Name: ");
+            String name = new Scanner(System.in).nextLine();
+            String max = scanner.next();
+
+            try{
+                Integer.parseInt(max);
+            }catch (NumberFormatException e){
+                displayNotAnOption();
+                return;
+            }
+
+            roomController.editRoom(currentLocation, room, name, max);
+
+            System.out.println("Room edited!");
+        }else{
+            displayNotAnOption();
+        }
+    }
+
+    private void seeRoomView(){
+        System.out.println("\n****************************");
+        System.out.println("Name: " + Database.currentLoggedInOrganizer.getLocations().get(currentLocation).getRooms().get(currentRoom).getName() +
+                "\nMax participents: " + Database.currentLoggedInOrganizer.getLocations().get(currentLocation).getRooms().get(currentRoom).getMaxParticipents() +
+                "\nEvent in progress: " + Database.currentLoggedInOrganizer.getLocations().get(currentLocation).getRooms().get(currentRoom).getEventInProgress());
+        System.out.println("Events in this room: ");
+        for(int i = 0; i < Database.currentLoggedInOrganizer.getLocations().get(currentLocation).getRooms().get(currentRoom).getEvents().size(); i++)
+            System.out.println("\t(" + i + ") " + Database.currentLoggedInOrganizer.getLocations().get(currentLocation).getRooms().get(currentRoom).getEvents().get(i).getNameOfEvent());
+        System.out.println("****************************");
+
+        System.out.println("\nWhat would you like to do?");
+        System.out.println("\t(1) See event");
+        System.out.println("\t(2) Add event");
+        System.out.println("\t(3) Remove event");
+        System.out.println("\t(4) Edit event");
+        System.out.println("\t(5) Go back");
+
+        int choice = 0;
+        try{
+            choice = scanner.nextInt();
+        }catch (InputMismatchException e){
+            displayNotAnOption();
+            seeRoomView();
+        }
+
+        switch (choice){
+            case 1:
+                seeEvent();
+                break;
+            case 2:
+                addEvent();
+                break;
+            case 3:
+                removeEvent();
+                break;
+            case 4:
+                editEvent();
+                break;
+            case 5:
+                currentRoom = -1;
+                seeLocationView();
+                break;
+            default:
+                displayNotAnOption();
+                seeRoomView();
+                break;
+        }
+        seeRoomView();
+
+    }
+
+    private void seeEvent() {
+
+    }
+    private void addEvent() {
+
+    }
+    private void removeEvent() {
+
+    }
+    private void editEvent() {
+
+    }
+
 
     private void displayNotAnOption(){
         System.out.println("Sorry, that is not an option");
--- src/main/java/View/OrganizerMenuView.java
+++ src/main/java/View/OrganizerMenuView.java
@@ -4,90 +4,16 @@ import Dummy.Database;
 
 public class OrganizerMenuView {
 
-    public void displayPromptForSpesificLocationMenu() {
-        System.out.println("\nWhat would you like to do?");
-        System.out.println("\t(0) See a room");
-        System.out.println("\t(1) Delete a room");
-        System.out.println("\t(2) Add a room");
-        System.out.println("\t(3) Edit a room");
-        System.out.println("\t(4) Go back");
-    }
 
-    public void displayPromptForRoomName() {
-        System.out.println("\nRoom name: ");
-    }
-
-    public void displayPromptForMaxParticipents() {
-        System.out.println("Max participents:");
-    }
-
-    public void displayPromptForRoomAdded() {
-        System.out.println("Room added...");
-    }
-
-    public void displayPromptForWhichRoomToDelete() {
-        System.out.println("Which room do you want to delete?");
-    }
-
-    public void displayPromptForRoomRemoved() {
-        System.out.println("Room removed");
-    }
-
-    public void displayPromptForSeeingSpesificRoom() {
-        System.out.println("Which room do you want to see?");
-    }
-
-    public void displayPromptForRoomDetails(int location, int room) {
-        System.out.println("\n****************************");
-        System.out.println("Name: " + Database.currentLoggedInOrganizer.getLocations().get(location).getRooms().get(room).getName() +
-                "\nMax participents: " + Database.currentLoggedInOrganizer.getLocations().get(location).getRooms().get(room).getMaxParticipents() +
-                "\nEvent in progress: " + Database.currentLoggedInOrganizer.getLocations().get(location).getRooms().get(room).getEventInProgress());
-    }
-
-    public void displayPromptForNotEditField() {
-        System.out.println("\nWrite \"-1\" if you dont want to edit a field");
-    }
-
-    public void displayPromptForNewRoomName() {
-        System.out.println("Name: ");
-    }
-
-    public void displayPromptForNewMaxParticipents() {
-        System.out.println("Max participents: ");
-    }
-
-    public void displayPromptForRoomEdited() {
-        System.out.println("Room edited");
-    }
-
-    public void displayPromptForWhichRoomToEdit() {
-        System.out.println("Which room?");
-    }
 
     public void displayPromptForEventRemoved() {
         System.out.println("Event removed");
-    }
-
-    public void displayPromptForStars() {
-        System.out.println("****************************");
     }
 
     public void displayPromptForWhichEventToRemove() {
         System.out.println("Which event do you want to remove?");
     }
 
-    public void displayPromptForEventsInRoom() {
-        System.out.println("Events in this room: ");
-    }
-
-    public void displayPromptForEventMenu() {
-        System.out.println("\nWhat would you like to do?");
-        System.out.println("\t(0) See event");
-        System.out.println("\t(1) Add event");
-        System.out.println("\t(2) Remove event");
-        System.out.println("\t(3) Edit event");
-        System.out.println("\t(4) Go back");
-    }
 
     public void displayPromptForSeeEvent() {
         System.out.println("Which event do you want to see?");
