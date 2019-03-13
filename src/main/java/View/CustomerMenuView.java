5b068ed6c1ebd2d8a7a04b6d4c6c8f7d305f1f72
Andreas Nielsen <andreni@hiof.no>
2019-03-13 22:15:57 +0100

    Added todo

--- src/main/java/View/CustomerMenuView.java
+++ src/main/java/View/CustomerMenuView.java
@@ -15,11 +15,13 @@ public class CustomerMenuView {
         customerMenuController.enterCustomerMenu();
         System.out.println("Please enter the event number of the performance you wish to purchase tickets to.");
         String eventNumberInput = scanner.next();
+        // TODO Move validation to controller
         if(eventNumberInput.length()<4) {
             System.out.println("Invalid selection entered");
                 promptUserForAEventSeletion();
             }else{
                 customerMenuController.validateUserSelection(eventNumberInput);
+                // TODO Send user to OrderView
             }
         }
     }
