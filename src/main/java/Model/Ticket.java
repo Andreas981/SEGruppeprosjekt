f2ae4c894543a7537688ba954ea4a4be36ff6d25
Andreas Mikalsen <amikal@hotmail.no>
2019-03-16 13:40:08 +0100

    Edited Ticket model

--- src/main/java/Model/Ticket.java
+++ src/main/java/Model/Ticket.java
@@ -1,22 +1,25 @@
 package Model;
 
+import Controller.NonSeatedEventController;
+
 public class Ticket {
     private String idForTicket; // Ticket serial number used for seatedEvents
     private PlannedEvent validForEvent; // The event the ticket is valid for
     private int priceInNOK;
-    private String seatNumber;
+    private int seatNumber;
+    private Boolean availible;
 
     // Used for Non seated events
     public Ticket(PlannedEvent validForEvent, int priceInNOK) {
         this.validForEvent = validForEvent;
         this.priceInNOK = priceInNOK;
+        this.availible = true;
     }
 
     // Used for Seated events
-    public Ticket(String idForTicket, PlannedEvent validForEvent, int priceInNOK, String seatNumber) {
+    public Ticket(String idForTicket, PlannedEvent validForEvent, int priceInNOK, int seatNumber) {
+        this(validForEvent, priceInNOK);
         this.idForTicket = idForTicket;
-        this.validForEvent = validForEvent;
-        this.priceInNOK = priceInNOK;
         this.seatNumber = seatNumber;
     }
 }
