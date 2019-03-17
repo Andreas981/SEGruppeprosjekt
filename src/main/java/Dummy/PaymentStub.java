f0be12b6d2cd23d416b5cb328fa4c1741987c692
Andreas Mikalsen <amikal@hotmail.no>
2019-03-17 16:49:31 +0100

    Added payment stub for Pay Pal payments

--- src/main/java/Dummy/PaymentStub.java
+++ src/main/java/Dummy/PaymentStub.java
@@ -1,5 +1,9 @@
 package Dummy;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class PaymentStub {
     /*
     PayPal
@@ -7,25 +11,51 @@ public class PaymentStub {
     Pay at desk
      */
 
+    public Boolean payPal(String username){
+        String hashed;
 
-    
+        try{
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.reset();
+            md.update(username.getBytes());
 
+            byte[] digest = md.digest();
 
+            BigInteger bigInt = new BigInteger(1, digest);
 
+            hashed = bigInt.toString(16);
 
+        }catch (NoSuchAlgorithmException e){
+            errorDatabaseConnectionFail();
+            return false;
+        }
 
-    private void errorNotEnoughtMoney(){
-        System.out.println("Sorry, payment faile due to account balance.");
+        String[] numbers = hashed.replaceAll("\\D+","").split("");
+        int sum = 0;
+        for(int i = 0; i < numbers.length; i++)
+            if(numbers[i].equals("1") || numbers[i].equals("3"))
+                sum ++;
+
+        if(sum % 3 == 0) {
+            errorWrongPinCode();
+            return false;
+        }else if(sum % 4 == 0){
+            errorDatabaseConnectionFail();
+            return false;
+        }else{
+            success();
+            return true;
+        }
     }
+
+
 
     private void errorDatabaseConnectionFail(){
         System.out.println("Sorry, payment failed due to server issues.");
     }
-
     private void errorWrongPinCode(){
         System.out.println("Sorry, payment failed due to wrong pin code");
     }
-
     private void success(){
         System.out.println("Payment Successfull. Thanks for the order.");
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         //Initializing the dummy database
         InitStart.Init();
         new MainMenuView().displayMenu();
