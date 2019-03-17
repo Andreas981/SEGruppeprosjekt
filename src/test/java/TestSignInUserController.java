f4835082536de6f658f9e437681224ed1ce343bf
Herman <Hermannl@hiof.no>
2019-03-17 15:38:31 +0100

    Created & finished TestSignInUserController.java

---
+++ src/test/java/TestSignInUserController.java
@@ -0,0 +1,36 @@
+import Dummy.Database;
+import Model.Customer;
+import Model.Organizer;
+import org.joda.time.LocalDate;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestSignInUserController {
+
+    Organizer organizer;
+    Customer customer;
+
+    @Before
+    public void init(){
+
+        organizer = new Organizer("test name","test address","test email","12312312",
+                "test username", "test password", new LocalDate(1991,1,1),"test org",2);
+        customer = new Customer("test name","test address","test email","12312312",
+                "test username", "test password", new LocalDate(1991,1,1));
+        Database.currentLoggedInOrganizer = organizer;
+        Database.currentLoggedInCustomer = customer;
+
+    }
+
+    @Test
+    public void testSignInOrganizer() {
+        Assert.assertEquals("test name",Database.currentLoggedInOrganizer.getFirstName());
+    }
+
+    @Test
+    public void testSignInCustomer() {
+        Assert.assertEquals("test name",Database.currentLoggedInCustomer.getFirstName());
+    }
+
+}
