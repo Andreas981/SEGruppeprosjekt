42f9a3f6940e3cb4779071aa41ba593d79a90655
Andreas Nielsen <andreni@hiof.no>
2019-03-17 15:56:12 +0100

    Moved payment menu selection to view

--- src/main/java/Controller/PaymentController.java
+++ src/main/java/Controller/PaymentController.java
@@ -18,34 +18,9 @@ public class PaymentController {
         return amount;
     }
 
-    public void selectedOption(int userSelection){
-        try{
-            switch (userSelection) {
-                case 1:
-                    System.out.println("PayPal Selected");
-                case 2:
-                    System.out.println("Debitcard selected");
-                    break;
-                case 3:
-                    System.out.println("Pay at desk selected");
-                    break;
-                case 4:
-                    System.out.println("Goodbye");
-                    return;
-                default:
-                    printMenu();
-            }
-        }catch (InputMismatchException e){
-            System.out.println("Sorry, that is not an option");
-        }
-    }
 
-    public void printMenu() {
-        System.out.println("\nPlease enter one of the following options:");
-        System.out.println("\tType '1' to pay with PayPal");
-        System.out.println("\tType '2' to pay with DebitCard");
-        System.out.println("\tType '3' to pay at desk");
-        System.out.println("\tType '4' to quit");
-    }
+
+
+
 
 }
--- src/main/java/View/PaymentView.java
+++ src/main/java/View/PaymentView.java
@@ -3,6 +3,7 @@ package View;
 import Controller.PaymentController;
 import Model.Order;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class PaymentView {
@@ -23,8 +24,41 @@ public class PaymentView {
 
     public void displayPaymentOptions(){
         System.out.println("How would you like to pay?");
-        paymentController.printMenu();
-        paymentController.selectedOption(scanner.nextInt());
+        printMenu();
+        selectedOption(scanner.nextInt());
+    }
 
+    public void selectedOption(int userSelection) {
+        try {
+            switch (userSelection) {
+                case 1:
+                    System.out.println("PayPal Selected");
+                    break;
+                case 2:
+                    System.out.println("Debitcard selected");
+                    break;
+                case 3:
+                    System.out.println("Pay at desk selected");
+                    break;
+                case 4:
+                    System.out.println("Goodbye");
+                    return;
+                default:
+                    System.out.println("I did not get that...");
+                    printMenu();
+                    return;
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Sorry, that is not an option");
+
+        }
+    }
+
+    public void printMenu() {
+        System.out.println("\nPlease enter one of the following options:");
+        System.out.println("\tType '1' to pay with PayPal");
+        System.out.println("\tType '2' to pay with DebitCard");
+        System.out.println("\tType '3' to pay at desk");
+        System.out.println("\tType '4' to quit");
     }
 }
