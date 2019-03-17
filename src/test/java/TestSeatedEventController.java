63b97cf05115d922b89c590952f52033a4d07606
Herman <Hermannl@hiof.no>
2019-03-17 15:01:21 +0100

    Finished TestSeatedEventController.java
    fixed issue regarding adding location to database

--- src/test/java/TestSeatedEventController.java
+++ src/test/java/TestSeatedEventController.java
@@ -1,5 +1,6 @@
 import Dummy.Database;
 import Model.*;
+import org.joda.time.LocalDate;
 import org.joda.time.LocalDateTime;
 import org.junit.Assert;
 import org.junit.Before;
@@ -10,6 +11,7 @@ public class TestSeatedEventController {
     Location location;
     SeatedPlannedEvent seatedPlannedEvent;
     Room room;
+    Organizer organizer;
     Database database;
 
     @Before
@@ -18,16 +20,17 @@ public class TestSeatedEventController {
         location = new Location("test name","test address",false);
         seatedPlannedEvent = new SeatedPlannedEvent("test name", new LocalDateTime(16),1,18);
         room = new Room("test room name", 123,false);
-
+        organizer = new Organizer("test name","test address","test email","12312312",
+                "test username", "test password", new LocalDate(1991,1,1),"test org",1);
         room.addEvent(seatedPlannedEvent);
         location.addRoom(room);
+        Database.currentLoggedInOrganizer = organizer;
         Database.currentLoggedInOrganizer.addLocation(location);
-
     }
 
     @Test
     public void TestAddSeatedEvent() {
         /* Checks if array's size is not null/0 */
-        Assert.assertNotEquals(null,location.getRooms().get(0).getEvents().get(0));
+        Assert.assertNotEquals(null,Database.currentLoggedInOrganizer.getLocations().get(0).getRooms().get(0).getEvents().get(0));
     }
 }
