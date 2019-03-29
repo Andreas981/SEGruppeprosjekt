ed78f5a62398d2cd777087f19d84aa659f5c0212
Andreas Nielsen <andreni@hiof.no>
2019-03-29 14:15:22 +0100

    Started rework of displaying events

--- src/main/java/Controller/CustomerMenuController.java
+++ src/main/java/Controller/CustomerMenuController.java
@@ -8,30 +8,21 @@ public class CustomerMenuController {
     public static void displayOnGoingEvents(){
         // For use in main screen
         System.out.println("_____________________________________________________________________");
-        for (Organizer organizer : Database.organizers){
+
             System.out.println("*********************************************************************");
-            System.out.println("Arranger: " + organizer.getOrganization());
-            // TODO Add listing of nonSeated events
-            // for(Event nonSeated : organizer.a)
-            for(Location location : organizer.getLocations()){
-                System.out.println("    Playing at: " + location.getName());
-                for(Room aRoom : location.getRooms()){
-                    System.out.println("        In room: " + aRoom.getName());
-                    if(aRoom.getEvents().size()<1){
-                        System.out.println("            No planned events");
-                    }else{
-                        for(Model.PlannedEvent aEvent : aRoom.getEvents()){
-                            System.out.println("            " + aEvent.getNameOfEvent()
-                                    + " " + aEvent.getDateOfEvent().toLocalDate().toString() + " \n" +
-                                    "               Starting at: " + aEvent.getDateOfEvent().toLocalTime().getHourOfDay() + ":" +aEvent.getDateOfEvent().getMinuteOfHour() + " "
-                            + aEvent.getLengthOfEvent() + "min runtime ");
-                        }
-                    }
+
+             for(newEvent upcomingEvent : Database.EventPool){
+
+                System.out.println("    Playing at: " + upcomingEvent.getAddress());
+                if(!upcomingEvent.getFreeEvent()){
 
                 }
-            }
+                 System.out.println( upcomingEvent.getTickets().size());
+                 System.out.println();
 
-        }
+                
+             }
+
         System.out.println("_____________________________________________________________________");
         System.out.println("To buy a ticket for a event, please log in");
     }
