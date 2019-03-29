c34b2ca17d9b2cff81554054b5e9cc955df31dca
Andreas Nielsen <andreni@hiof.no>
2019-03-29 14:04:57 +0100

    Added event pool

--- src/main/java/Dummy/Database.java
+++ src/main/java/Dummy/Database.java
@@ -2,12 +2,14 @@ package Dummy;
 
 import Model.Customer;
 import Model.Organizer;
+import Model.newEvent;
 
 import java.util.ArrayList;
 
 public class Database {
     public static ArrayList<Organizer> organizers = new ArrayList<Organizer>();
     public static ArrayList<Customer> customers = new ArrayList<Customer>();
+    public static ArrayList<newEvent> EventPool = new ArrayList<newEvent>();
 
     public static Organizer currentLoggedInOrganizer = null;
     public static Customer currentLoggedInCustomer = null;
