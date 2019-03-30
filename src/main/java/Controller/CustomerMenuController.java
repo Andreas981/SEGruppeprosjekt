b4f7ff76bf801be38100e4d48933d4c23f4489e8
Andreas Nielsen <andreni@hiof.no>
2019-03-30 14:12:49 +0100

    Removed unused methods

--- src/main/java/Controller/CustomerMenuController.java
+++ src/main/java/Controller/CustomerMenuController.java
@@ -37,42 +37,12 @@ public class CustomerMenuController {
         if(userInput.length()<1){
             return false;
         }
-        String[] eventSplit = userInput.split("");
-        int[] eventNumber = new int[eventSplit.length];
-        try  {
-            for (int i = 0; i < eventSplit.length; i++) {
-                eventNumber[i] = Integer.parseInt(eventSplit[i]);
-            }
-        }catch (NumberFormatException e){
-            System.out.println("Invalid selection entered");
-            return false;
-        }
-        if(checkIfEventExist(eventNumber)){
-            return true;
-        }
-        return null;
+        return false;
     }
 
     // Control that that valid int selection exist in the database
-    private Boolean checkIfEventExist(int[] eventNumber) {
-
-        if(Database.organizers.size()>eventNumber[0]){
-
-            if (Database.organizers.get(eventNumber[0]).getLocations().size()>eventNumber[1]) {
-
-                if (Database.organizers.get(eventNumber[0]).getLocations().get(eventNumber[1]).getRooms().size()>eventNumber[2]){
-                    return Database.organizers.get(eventNumber[0]).getLocations().get(eventNumber[1]).getRooms()
-                            .get(eventNumber[2]).getEvents().size() > (eventNumber[3]);
-                }
-                return false;
-            }
-            return false;
-        }
+    private Boolean checkIfEventExist(int eventNumber) {
+        // TODO Implement check for checking that the event exist
         return false;
-
-    }
-
-    private void selectEvent(int[] eventNumber){
-
     }
 }
