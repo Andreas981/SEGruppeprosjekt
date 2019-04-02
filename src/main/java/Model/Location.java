8e7b960fe51f3074674713ce3c8782b4b23c18aa
Andreas Mikalsen <amikal@hotmail.no>
2019-04-02 14:05:28 +0100

    Removed test comment

--- src/main/java/Model/Location.java
+++ src/main/java/Model/Location.java
@@ -12,7 +12,6 @@ public class Location {
     Boolean publicLocation;
     ArrayList<Room> rooms;
     //TODO Add accounting for location
-    //TODO
 
     public Location(String name, String address, Boolean publicLocation) {
         this.name = name;
