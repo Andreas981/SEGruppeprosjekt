537e528605f4b3bef667a74fa4747fdf0f426445
Andreas Mikalsen <amikal@hotmail.no>
2019-04-06 10:32:40 +0200

    Test for payment stub

---
+++ src/test/java/TestStubPaymentStub.java
@@ -0,0 +1,20 @@
+import Dummy.PaymentStub;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestStubPaymentStub {
+
+    PaymentStub stub;
+
+    @Before
+    public void init(){
+        stub = new PaymentStub();
+    }
+
+    @Test
+    public void paymentFromPayPalAccount(){
+        Assert.assertTrue(stub.payPal("andremi"));
+    }
+
+}
